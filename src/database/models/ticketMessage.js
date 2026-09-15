6d8d55fc2c88a3b43e8a2d338f5d547505ebadd6
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-15 12:32:56 +0530

    Add persistent ticket panel editor configuration

--- src/database/models/ticketMessage.js
+++ src/database/models/ticketMessage.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    Guild: String,
-    openTicket: String,
-    dmMessage: String
+  Guild: String,
+  openTicket: String,
+  dmMessage: String,
+  panelTitle: { type: String, default: "Need help? Open a ticket!" },
+  panelDescription: { type: String, default: "Choose a ticket category below and our support team will help you." },
+  panelChannel: { type: String, default: null },
+  panelMessageId: { type: String, default: null },
 });
 
 module.exports = mongoose.model("ticketMessage", Schema);
