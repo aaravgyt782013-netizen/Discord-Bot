00a24f9f964ed2b67fcb2b3a454ec847bd5eda31
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-15 12:08:26 +0530

    brand LightCore config and update Discord links

--- src/config/bot.js
+++ src/config/bot.js
@@ -7,10 +7,10 @@ module.exports = {
 
     discord: {
         id: process.env.DISCORD_ID,
-        prefix: '!',
-        footer: `© Corwin 2021 - ${new Date().getFullYear()}`, 
-        botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
-        serverInvite: "https://discord.gg/qHXxpQKC2H",
+        prefix: '.',
+        footer: `© LightCore ${new Date().getFullYear()}`,
+        botInvite: `https://discord.com/oauth2/authorize?client_id=1516313619476779120&permissions=8&scope=bot%20applications.commands`,
+        serverInvite: "https://discord.gg/Ehmqr5drSz",
     },
 
     wordList: `Airplane
@@ -1063,6 +1063,4 @@ you
 young
 your
 yourself`
-}
-
- 
+};
