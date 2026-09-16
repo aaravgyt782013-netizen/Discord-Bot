652b1dbb9f5edcfd4951989620ed0edb422374ea
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-16 15:56:37 +0530

    Redirect legacy rank command to unified leveling

--- src/commands/levels/rank.js
+++ src/commands/levels/rank.js
@@ -2,51 +2,36 @@ const Discord = require("discord.js");
 const Canvacord = require("canvacord");
 
 const Functions = require("../../database/models/functions");
-const Schema = require("../../database/models/levels");
+const leveling = require("../../database/models/leveling");
+const levelingService = require("../../handlers/games/levelingService");
 
-/**
- * @type {import("../../typings.d").Command}
- */
 module.exports = async (client, interaction, args) => {
   const data = await Functions.findOne({ Guild: interaction.guild.id });
-
-  if (data && data.Levels == true) {
-    const target = interaction.options.getUser("user") || interaction.user;
-    const user = await client.fetchLevels(target.id, interaction.guild.id);
-    if (!user || !user.xp)
-      return client.errNormal(
-        {
-          error: "This user has no levels!",
-          type: "editreply",
-        },
-        interaction,
-      );
-    let xpRequired = client.xpFor(user.level + 1);
-
-    const rankCard = new Canvacord.Rank()
-      .setAvatar(target.displayAvatarURL({ dynamic: false, extension: "png" }))
-      .setRequiredXP(xpRequired)
-      .setCurrentXP(user.xp)
-      .setLevel(user.level)
-      .setProgressBar(client.config.colors.normal, "COLOR")
-      .setUsername(target.username)
-      .setDiscriminator(target.discriminator)
-      .setStatus("dnd")
-      .setRank(user.position);
-
-    rankCard.build().then((data) => {
-      const attachment = new Discord.AttachmentBuilder(data, {
-        name: "RankCard.png",
-      });
-      interaction.editReply({ files: [attachment] });
-    });
-  } else {
-    client.errNormal(
-      {
-        error: "Levels are disabled in this guild!",
-        type: "editreply",
-      },
-      interaction,
-    );
+  if (!(data && data.Levels == true)) {
+    return client.errNormal({ error: "Levels are disabled in this guild!", type: "editreply" }, interaction);
   }
+
+  await levelingService.ensureLegacyMigration();
+  const target = interaction.options.getUser("user") || interaction.user;
+  const record = await leveling.findOne({ Guild: interaction.guild.id, User: target.id }).exec();
+  if (!record || record.XP <= 0) {
+    return client.errNormal({ error: "This user has no levels!", type: "editreply" }, interaction);
+  }
+
+  const config = await levelingService.getConfig(interaction.guild.id);
+  const position = await leveling.countDocuments({ Guild: interaction.guild.id, XP: { $gt: record.XP } }) + 1;
+  const xpRequired = levelingService.xpFor(record.Level + 1, config.XPMultiplier);
+  const rankCard = new Canvacord.Rank()
+    .setAvatar(target.displayAvatarURL({ dynamic: false, extension: "png" }))
+    .setRequiredXP(xpRequired)
+    .setCurrentXP(record.XP)
+    .setLevel(record.Level)
+    .setProgressBar(client.config.colors.normal, "COLOR")
+    .setUsername(target.username)
+    .setDiscriminator(target.discriminator)
+    .setStatus("dnd")
+    .setRank(position);
+
+  const image = await rankCard.build();
+  interaction.editReply({ files: [new Discord.AttachmentBuilder(image, { name: "RankCard.png" })] });
 };
