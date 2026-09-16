5226266ddb936ced63c8f976166095dd1527b049
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-16 15:56:56 +0530

    Redirect set XP to unified leveling model

--- src/commands/levels/setxp.js
+++ src/commands/levels/setxp.js
@@ -1,57 +1,29 @@
 const Discord = require("discord.js");
-const Canvacord = require("canvacord");
+const leveling = require("../../database/models/leveling");
+const levelingService = require("../../handlers/games/levelingService");
 
-const Functions = require("../../database/models/functions");
-const Schema = require("../../database/models/levels");
-
-/**
- * @type {import("../../typings.d").Command}
- */
 module.exports = async (client, interaction, args) => {
-  const data = await Functions.findOne({ Guild: interaction.guild.id });
-
   const perms = await client.checkUserPerms(
-    {
-      flags: [Discord.PermissionsBitField.Flags.ManageMessages],
-      perms: [Discord.PermissionsBitField.Flags.ManageMessages],
-    },
+    { flags: [Discord.PermissionsBitField.Flags.ManageMessages], perms: [Discord.PermissionsBitField.Flags.ManageMessages] },
     interaction,
   );
-
   if (perms == false) return;
 
-  if (data && data.Levels == true) {
-    const target = interaction.options.getUser("user");
-    const xp = interaction.options.getNumber("amount");
+  const target = interaction.options.getUser("user");
+  const xp = interaction.options.getNumber("amount");
+  await levelingService.ensureLegacyMigration();
+  const config = await levelingService.getConfig(interaction.guild.id);
+  const user = await leveling.findOneAndUpdate(
+    { Guild: interaction.guild.id, User: target.id },
+    { $set: { XP: Math.max(0, xp), Level: levelingService.levelFor(Math.max(0, xp), config.XPMultiplier), LastUpdated: new Date() } },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+  ).exec();
 
-    const user = await client.setXP(target.id, interaction.guild.id, xp);
-
-    client.succNormal(
-      {
-        text: `XP has been modified successfully`,
-        fields: [
-          {
-            name: "🆕┆New XP",
-            value: `${user.xp}`,
-            inline: true,
-          },
-          {
-            name: "👤┆User",
-            value: `${target} (${target.tag})`,
-            inline: true,
-          },
-        ],
-        type: "editreply",
-      },
-      interaction,
-    );
-  } else {
-    client.errNormal(
-      {
-        error: "Levels are disabled in this guild!",
-        type: "editreply",
-      },
-      interaction,
-    );
-  }
+  client.succNormal(
+    { text: "XP has been modified successfully", fields: [
+      { name: "🆕┆New XP", value: `${user.XP}`, inline: true },
+      { name: "👤┆User", value: `${target} (${target.tag})`, inline: true },
+    ], type: "editreply" },
+    interaction,
+  );
 };
