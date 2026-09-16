aa2eb93ae456a26b9f6a7ab5e3abf5de1db1b17a
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-16 15:19:53 +0530

    feat: add configurable automod thresholds

--- src/database/models/functions.js
+++ src/database/models/functions.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    Guild: String,
-    Levels: { type: Boolean, default: false },
-    Beta: { type: Boolean, default: false },
-    AntiAlt: { type: Boolean, default: false },
-    AntiSpam: { type: Boolean, default: false },
-    AntiCaps: { type: Boolean, default: false },
-    AntiInvite: { type: Boolean, default: false },
-    AntiLinks: { type: Boolean, default: false },
-    Prefix: String,
-    Color: String 
-});
+  Guild: { type: String, required: true, unique: true, index: true },
+  Levels: { type: Boolean, default: false },
+  Beta: { type: Boolean, default: false },
+  AntiAlt: { type: Boolean, default: false },
+  AntiSpam: { type: Boolean, default: false },
+  AntiCaps: { type: Boolean, default: false },
+  AntiInvite: { type: Boolean, default: false },
+  AntiLinks: { type: Boolean, default: false },
+  Prefix: { type: String, default: "." },
+  Color: String,
+  SpamLimit: { type: Number, default: 5, min: 2, max: 20 },
+  SpamWindow: { type: Number, default: 10000, min: 2000, max: 60000 },
+  CapsPercentage: { type: Number, default: 70, min: 50, max: 100 },
+  MassMentionLimit: { type: Number, default: 5, min: 2, max: 20 },
+}, { timestamps: true });
 
 module.exports = mongoose.model("functions", Schema);
