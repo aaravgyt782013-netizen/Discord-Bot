89cc2ada8836ed7d2d28e886208c2a2eb8a81ab3
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-16 15:57:02 +0530

    Redirect set level to unified leveling model

--- src/commands/levels/setlevel.js
+++ src/commands/levels/setlevel.js
@@ -1,57 +1,28 @@
 const Discord = require("discord.js");
-const Canvacord = require("canvacord");
+const leveling = require("../../database/models/leveling");
+const levelingService = require("../../handlers/games/levelingService");
 
-const Functions = require("../../database/models/functions");
-const Schema = require("../../database/models/levels");
-
-/**
- * @type {import("../../typings.d").Command}
- */
 module.exports = async (client, interaction, args) => {
-  const data = await Functions.findOne({ Guild: interaction.guild.id });
-
   const perms = await client.checkUserPerms(
-    {
-      flags: [Discord.PermissionsBitField.Flags.ManageMessages],
-      perms: [Discord.PermissionsBitField.Flags.ManageMessages],
-    },
+    { flags: [Discord.PermissionsBitField.Flags.ManageMessages], perms: [Discord.PermissionsBitField.Flags.ManageMessages] },
     interaction,
   );
-
   if (perms == false) return;
 
-  if (data && data.Levels == true) {
-    const target = interaction.options.getUser("user");
-    const level = interaction.options.getNumber("level");
+  const target = interaction.options.getUser("user");
+  const level = Math.max(0, interaction.options.getNumber("level"));
+  await levelingService.ensureLegacyMigration();
+  const user = await leveling.findOneAndUpdate(
+    { Guild: interaction.guild.id, User: target.id },
+    { $set: { Level: level, LastUpdated: new Date() } },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+  ).exec();
 
-    const user = await client.setLevel(target.id, interaction.guild.id, level);
-
-    client.succNormal(
-      {
-        text: `Level has been modified successfully`,
-        fields: [
-          {
-            name: "🆕┆New Level",
-            value: `${user.level}`,
-            inline: true,
-          },
-          {
-            name: "👤┆User",
-            value: `${target} (${target.tag})`,
-            inline: true,
-          },
-        ],
-        type: "editreply",
-      },
-      interaction,
-    );
-  } else {
-    client.errNormal(
-      {
-        error: "Levels are disabled in this guild!",
-        type: "editreply",
-      },
-      interaction,
-    );
-  }
+  client.succNormal(
+    { text: "Level has been modified successfully", fields: [
+      { name: "🆕┆New Level", value: `${user.Level}`, inline: true },
+      { name: "👤┆User", value: `${target} (${target.tag})`, inline: true },
+    ], type: "editreply" },
+    interaction,
+  );
 };
