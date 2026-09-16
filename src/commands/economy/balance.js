037a8b544212387263ca92e0933fbf3f6e2f1290
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-16 17:56:59 +0530

    cleanup: remove guild filter from global economy balance

--- src/commands/economy/balance.js
+++ src/commands/economy/balance.js
@@ -17,7 +17,7 @@ module.exports = async (client, interaction, args) => {
       interaction,
     );
 
-  Schema.findOne({ Guild: interaction.guild.id, User: user.id }).then(
+  Schema.findOne({ User: user.id }).then(
     async (data) => {
       if (data) {
         let total = data.Money + data.Bank;
